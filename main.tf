6f1b195eff4743b2eb3b855ce75d8bbbb1d818bf
Stephen Donovan <steviedons@gmail.com>
2017-10-19 17:45:39 +0100

    Added a security key, user data for a basic web server and opened a port in the security group

--- main.tf
+++ main.tf
@@ -3,10 +3,32 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-	ami 	      = "ami-62d7c206"
+    # This is the Ubuntu ami
+	ami 	      = "ami-996372fd"
 	instance_type = "t2.nano"
-
-	tags {
+    key_name      = "steve-titan"
+    user_data     = <<-EOF
+                    #!/bin/bash
+                    echo "Hello, World" > index.html
+                    nohup busybox httpd -f -p 8080 &
+                    EOF
+    
+    tags {
 		Name = "terraform-example"
      	}
+}
+
+resource "aws_security_group" "instance" { 
+    name = "terraform-example-instance"
+    ingress { 
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+}
+
+resource "aws_key_pair" "steve-titan" {
+  key_name   = "steve-titan"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/3ZU3FLKqkK+lNO/Uzuzvnexc6CDSYaEC5U+ndSVAJW8UuxWZSVY34MwrSagSex7EW3M/uBI+twXIB/pqER/OjtUkrw8AV8SbX2EU1SAGquMUw6njUK51loDd5T42xuavdKR9tn8yfW8mmDmKZe/ZyXPhlKkcZ5xvwqjdi0JWp9VP58FV64Iq+rb805icUuLqVI0L/EaeVNgbM7EXZdXVBom+sKXnvh8OE1STa11v8o/NHGf3/BRFQpqEe1ESgqQ936onWpD8e44brJjW9l8ZXhPDqMnVCu7kHQLhvF3kbSWTjr+OlabUKM5ZgfVrXxJmGaUpu18nca3Kk/XQoX03 steve@titan"
 }
