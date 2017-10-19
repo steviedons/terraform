abc5f4b75842595868591a0390f03e68ef657f6f
Stephen Donovan <steviedons@gmail.com>
2017-10-19 23:30:10 +0100

    Test of atoms git integration

--- main.tf
+++ main.tf
@@ -12,21 +12,22 @@ resource "aws_instance" "example" {
                     echo "Hello, World" > index.html
                     nohup busybox httpd -f -p 8080 &
                     EOF
-    
+
     tags {
 		Name = "terraform-example"
      	}
 }
 
-resource "aws_security_group" "instance" { 
+resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
-    ingress { 
+    ingress {
         from_port = 8080
         to_port = 8080
         protocol = "tcp"
         cidr_blocks = [ "0.0.0.0/0" ]
     }
 }
+
 
 resource "aws_key_pair" "steve-titan" {
   key_name   = "steve-titan"
