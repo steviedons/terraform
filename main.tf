586f3b9c4222d3e73af15f6f310783d6d966b850
Stephen Donovan <steviedons@gmail.com>
2017-10-20 16:28:44 +0100

    added the port number for the webserver as a variable

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
 	region = "eu-west-2"
 }
 
+variable "server_port" {
+	description = "The port the server will listen on"
+	default = 8080
+}
+
 resource "aws_instance" "example" {
     # This is the Ubuntu ami
 		ami   	      = "ami-996372fd"
@@ -11,7 +16,7 @@ resource "aws_instance" "example" {
     user_data     = <<-EOF
                     #!/bin/bash
                     echo "Hello, World" > index.html
-                    nohup busybox httpd -f -p 8080 &
+                    nohup busybox httpd -f -p "${var.server_port}" &
                     EOF
 
     tags {
@@ -22,8 +27,8 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
     ingress {
-        from_port = 8080
-        to_port = 8080
+				from_port = "${var.server_port}"
+	    	to_port = "${var.server_port}"
         protocol = "tcp"
         cidr_blocks = [ "0.0.0.0/0" ]
     }
